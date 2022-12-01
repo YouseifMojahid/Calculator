99d978b89b22789165842ceee72376accbc56d0f
Youseif Mojahid <youseifmojahidmain@gmail.com>
2022-12-01 17:46:45 +0200

    Second uplode

--- hello.c
+++ hello.c
@@ -3,27 +3,31 @@
 
 int main(void)
 {
-  float num1 = get_int("Write the first number: ");
-  char process = get_char("What do you want (+, -, *, /): ");
-  float num2 = get_int("Write the second number: ");
-  if (process == '+')
+  int i;
+  for (i = 0; i < 500; i++)
   {
-    printf("%f", num1 + num2);
-  }
-  else if (process == '-')
-  {
-    printf("%f", num1 - num2);
-  }
-  else if (process == '*')
-  {
-    printf("%f", num1 * num2);
-  }
-  else if (process == '/')
-  {
-    printf("%f", num1 / num2);
-  }
-  else
-  {
-    printf("Please write an oprator.");
+    float num1 = get_float("Write the first number: ");
+    char process = get_char("What do you want (+, -, *, /): ");
+    float num2 = get_float("Write the second number: ");
+    if (process == '+')
+    {
+      printf("%f\n", num1 + num2);
+    }
+    else if (process == '-')
+    {
+      printf("%f\n", num1 - num2);
+    }
+    else if (process == '*')
+    {
+      printf("%f\n", num1 * num2);
+    }
+    else if (process == '/')
+    {
+      printf("%f\n", num1 / num2);
+    }
+    else
+    {
+      printf("Please write an oprator.\n");
+    }
   }
 }
